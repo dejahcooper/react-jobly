528832df2573291d1bc674c4f756971aa8948165
Dejah Cooper <Dejah.C@icloud.com>
2026-01-31 12:37:24 -0500

    Add profile placeholder page

---
+++ jobly/src/components/Profile.tsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+  return (
+    <section className="page">
+      <h1>Edit Profile</h1>
+      <p>Profile editing form will go here.</p>
+    </section>
+  )
+}
+
+export default Profile
