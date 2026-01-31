c308bb6107f81e5ddd589607a49a0da95dff2fbd
Dejah Cooper <Dejah.C@icloud.com>
2026-01-31 12:37:24 -0500

    Add jobs list placeholder page

---
+++ jobly/src/components/JobsList.tsx
@@ -0,0 +1,10 @@
+const JobsList = () => {
+  return (
+    <section className="page">
+      <h1>Jobs</h1>
+      <p>List all jobs will appear here.</p>
+    </section>
+  )
+}
+
+export default JobsList
