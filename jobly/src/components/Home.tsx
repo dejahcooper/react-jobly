b9a1b1593f6584d5576fede2f828b2905f432a66
Dejah Cooper <Dejah.C@icloud.com>
2026-01-31 12:37:24 -0500

    Add home placeholder page

---
+++ jobly/src/components/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section className="page">
+      <h1>Welcome to Jobly</h1>
+      <p>Browse companies, explore jobs, and manage your profile.</p>
+    </section>
+  )
+}
+
+export default Home
