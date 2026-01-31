dae30675326cb3f84c346b170816f158955baaee
Dejah Cooper <Dejah.C@icloud.com>
2026-01-31 12:37:24 -0500

    Add login placeholder page

---
+++ jobly/src/components/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <section className="page">
+      <h1>Login</h1>
+      <p>Login and signup will be built here.</p>
+    </section>
+  )
+}
+
+export default Login
