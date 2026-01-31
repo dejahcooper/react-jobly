d38f2e0242166025cc053bc1deb36826aadfeabc
Dejah Cooper <Dejah.C@icloud.com>
2026-01-31 12:37:24 -0500

    Add companies list placeholder page

---
+++ jobly/src/components/CompaniesList.tsx
@@ -0,0 +1,10 @@
+const CompaniesList = () => {
+  return (
+    <section className="page">
+      <h1>Companies</h1>
+      <p>List all companies will appear here.</p>
+    </section>
+  )
+}
+
+export default CompaniesList
