5666c785d2a9f82cfd32964e64c39e78dc3502ce
Dejah Cooper <Dejah.C@icloud.com>
2026-02-01 20:37:41 -0500

    Update profile placeholder

--- jobly/src/components/Profile.tsx
+++ jobly/src/components/Profile.tsx
@@ -2,9 +2,9 @@ const Profile = () => {
   return (
     <section className="page">
       <h1>Edit Profile</h1>
-      <p>Profile editing form will go here.</p>
+      <p>Editing Form.</p>
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
