f5075334cea9a2bb80878ec244f8fdbd4a901e36
Dejah Cooper <Dejah.C@icloud.com>
2026-02-01 20:37:40 -0500

    Add auth api helpers

--- jobly/src/api/api.js
+++ jobly/src/api/api.js
@@ -20,7 +20,9 @@ class JoblyApi {
     //there are multiple ways to pass an authorization token, this is how you pass it in the header.
     //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
     const url = `${BASE_URL}/${endpoint}`;
-    const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+    const headers = JoblyApi.token
+        ? { Authorization: `Bearer ${JoblyApi.token}` }
+        : {};
     const params = (method === "get")
         ? data
         : {};
@@ -59,12 +61,31 @@ class JoblyApi {
     return res.jobs;
   }
 
+  /** Login with username/password. */
+
+  static async login(data) {
+    let res = await this.request("auth/token", data, "post");
+    return res.token;
+  }
+
+  /** Signup with user details. */
+
+  static async signup(data) {
+    let res = await this.request("auth/register", data, "post");
+    return res.token;
+  }
+
+  /** Get current user info by username. */
+
+  static async getCurrentUser(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
   // obviously, you'll add a lot here ...
 }
 
 // for now, put token ("testuser" / "password" on class)
-JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-    "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-    "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
+JoblyApi.token = null;
 
 export default JoblyApi;
