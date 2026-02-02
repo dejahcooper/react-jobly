fc207db1ccff7264e74b9b1af66a5630a2cdd07b
Dejah Cooper <Dejah.C@icloud.com>
2026-02-01 20:37:40 -0500

    Update nav for auth state

--- jobly/src/components/NavBar.tsx
+++ jobly/src/components/NavBar.tsx
@@ -1,6 +1,13 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import CurrentUserContext from '../context/CurrentUserContext'
 
-const NavBar = () => {
+type NavBarProps = {
+  onLogout: () => void
+}
+
+const NavBar = ({ onLogout }: NavBarProps) => {
+  const { currentUser } = useContext(CurrentUserContext)
   const navLinkClass = ({ isActive }: { isActive: boolean }) =>
     `nav-link${isActive ? ' active' : ''}`
 
@@ -18,15 +25,25 @@ const NavBar = () => {
           <NavLink to="/jobs" className={navLinkClass}>
             Jobs
           </NavLink>
-          <NavLink to="/login" className={navLinkClass}>
-            Login
-          </NavLink>
-          <NavLink to="/signup" className={navLinkClass}>
-            Signup
-          </NavLink>
-          <NavLink to="/profile" className={navLinkClass}>
-            Profile
-          </NavLink>
+          {currentUser ? (
+            <>
+              <NavLink to="/profile" className={navLinkClass}>
+                {currentUser.username}
+              </NavLink>
+              <button type="button" className="nav-link nav-button" onClick={onLogout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <NavLink to="/login" className={navLinkClass}>
+                Login
+              </NavLink>
+              <NavLink to="/signup" className={navLinkClass}>
+                Signup
+              </NavLink>
+            </>
+          )}
         </div>
       </nav>
     </header>
