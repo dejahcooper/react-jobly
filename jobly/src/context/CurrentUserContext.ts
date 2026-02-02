7f337d82b84a3458b8b4b156d989801fe3bc6c30
Dejah Cooper <Dejah.C@icloud.com>
2026-02-01 20:53:43 -0500

    Track applied jobs in user context

--- jobly/src/context/CurrentUserContext.ts
+++ jobly/src/context/CurrentUserContext.ts
@@ -6,16 +6,19 @@ type CurrentUser = {
   lastName?: string
   email?: string
   isAdmin?: boolean
+  jobs?: { id: number; state?: string }[]
 }
 
 type CurrentUserContextValue = {
   currentUser: CurrentUser | null
   isLoadingUser: boolean
+  applyToJob: (jobId: number) => Promise<void>
 }
 
 const CurrentUserContext = createContext<CurrentUserContextValue>({
   currentUser: null,
   isLoadingUser: true,
+  applyToJob: async () => undefined,
 })
 
 export default CurrentUserContext
